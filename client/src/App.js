86c613ff326bd1c7ae590b108dea4fc00ed86056
akileshrao <akileshrao19@gmail.com>
2021-04-18 09:30:25 +0530

    Initial

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://localhost:8000/login">Click me and login</a>
+    </div>
+  );
+}
+
+export default App;
